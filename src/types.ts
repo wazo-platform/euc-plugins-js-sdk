234261cca564b925d53af0e91fa8a663e500ac22
Emmanuel Quentin <manu.quentin@gmail.com>
2022-12-29 10:03:13 -0500

    Update SDK API

---
+++ src/types.ts
@@ -0,0 +1,153 @@
+export enum HostClientType {
+  desktop = 'desktop',
+  web = 'web',
+  android = 'android',
+  ios = 'ios',
+}
+
+export interface Extension {
+  context: string,
+  exten: string,
+  id: number,
+  links?: Array<{ href: string, rel: string }>,
+}
+
+export interface Endpoint {
+  id: number,
+  links: Array<{ href: string, rel: string }>,
+  username?: string,
+}
+
+export interface SipLine {
+  id: any;
+  uuid: string;
+  tenantUuid: string;
+  username: string;
+  secret: string;
+  type: string;
+  host: string;
+  options: string[][] | null;
+  endpointSectionOptions: string[][] | null;
+  links: Array<Object>;
+  trunk: string | null;
+  line: Endpoint;
+}
+
+export interface Line {
+  type: string;
+  id: number;
+  extensions: Array<Extension>;
+  endpointCustom: Endpoint | null;
+  endpointSccp: Endpoint | null;
+  endpointSip: Endpoint | null;
+}
+
+export interface ForwardOption {
+  destination: string;
+  enabled: boolean;
+  key: string;
+}
+
+export interface Profile {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  lines: Array<Line>;
+  sipLines: Array<SipLine>;
+  username: string;
+  mobileNumber: string;
+  forwards: Array<ForwardOption>;
+  doNotDisturb: boolean | null;
+  onlineCallRecordEnabled: boolean | null;
+  state: string | null;
+  ringSeconds:  number | null;
+  voicemail: { id: number, name: string } | null;
+  status: string;
+  subscriptionType:  number | null;
+  agent: { firstname: string, id: number, lastname: string, number: string } | null;
+  switchboards: Array<any>;
+  callPickupTargetUsers: Array<{ firstname: string, lastname: string, uuid: string }>
+}
+
+export interface Authorization {
+  uuid: string,
+  rules: Array<{ name: string, options: Object }>,
+  service: string | null,
+}
+
+export interface PortalSession {
+
+}
+
+export interface WDASession {
+  acl: string[];
+  token: string;
+  refreshToken: string | null;
+  uuid: string | null;
+  tenantUuid: string | null;
+  engineUuid: string | null;
+  sessionUuid: string | null;
+  engineVersion: string | null;
+  profile: Profile | null;
+  expiresAt: Date;
+  authorizations: Array<Authorization>;
+}
+
+// Wazo Session
+export type UserInfo = WDASession | PortalSession;
+
+export interface AppHostInfo {
+  clientType: HostClientType | null;
+}
+
+export interface AppInfo {
+  locale: string | null;
+  theme: Object | null;
+  host: AppHostInfo | null;
+  extra: Object | null;
+}
+
+export interface Context {
+  app: AppInfo;
+  user?: UserInfo;
+}
+
+export interface Call {
+  answered: boolean;
+  sipCallId: string;
+  callId: string;
+  sipStatus: string;
+  number: string;
+  displayName: string;
+  callerNumber: string;
+  creationTime: Date | null;
+  answerTime: Date | null;
+  endTime: Date | null;
+}
+
+export interface MeetingAuthorization {
+  meetingUuid: string;
+  uuid: string;
+  userUuid: string;
+  userName: string;
+  status: string;
+}
+
+export interface Meeting {
+  guestSipAuthorization: string;
+  uri: string;
+  uuid: string;
+  name: string;
+  port: string;
+  extension: string;
+  persistent: boolean;
+  ownerUuids: Array<string>;
+  creationTime: Date;
+  pendingAuthorizations: Array<MeetingAuthorization>;
+  requireAuthorization: boolean;
+}
+
+export interface Extra {
+  clientType: HostClientType;
+}
