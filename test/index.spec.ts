9162b5ffd8656cd0ca8ece40c02f7bd69c42fa15
Emmanuel Quentin <manu.quentin@gmail.com>
2023-02-16 13:27:37 -0500

    Fix unit tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,8 +3,9 @@ import { app } from '../src/index';
 window.parent.postMessage = jest.fn();
 
 test('expected message returned', () => {
+  app._initializeCompleted = true;
   app.startCall({ targets: ['*10'], requestedModalities: ['video'] });
 
-  const message = { type: 'wazo/START_CALL', targets: ['*10'], requestedModalities:  ['video'] };
+  const message = { type: 'wazo/START_CALL', targets: ['*10'], requestedModalities:  ['video'], _pluginId: null };
   expect(window.parent.postMessage).toHaveBeenCalledWith(message, '*');
 })
