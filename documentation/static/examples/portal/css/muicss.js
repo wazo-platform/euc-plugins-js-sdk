5f1671e0e24e0bbb94983463233b9948849a796b
Francis Chartrand <chartrandf@users.noreply.github.com>
2023-09-15 15:23:46 -0400

    docs: refactor muicss code (css for override)

--- documentation/static/examples/portal/css/muicss.js
+++ documentation/static/examples/portal/css/muicss.js
@@ -1,39 +1,35 @@
-import app from "https://cdn.jsdelivr.net/npm/@wazo/euc-plugins-sdk@0.0.20/lib/esm/app.js";
+import app from 'https://cdn.jsdelivr.net/npm/@wazo/euc-plugins-sdk@0.0.20/lib/esm/app.js';
 
-const generateWhitelabel = (theme) => {
-  if(theme) {
+const applyTheme = (theme) => {
+  if (theme) {
     const primary = theme.palette.primary;
     const secondary = theme.palette.secondary;
 
-    const whiteLabelStyle = document.createElement('style');
+    const themeStyle = document.createElement('style');
 
+    themeStyle.innerHTML = `
+      :root {
+        --primary-main: ${primary.main};
+        --primary-complementaryMain: ${primary.complementaryMain};
+        --primary-mainHover: ${primary.mainHover};
+        --primary-light: ${primary.light};
+        --primary-dark: ${primary.dark};
+        --primary-complementaryDark: ${primary.complementaryDark};
+        --primary-contrastText: ${primary.contrastText};
 
-    whiteLabelStyle.innerHTML = `
-      /* Primary */
-      .mui-btn--primary, .mui-btn--primary:active, .mui-btn--primary:hover, .mui-btn--primary:focus {
-        background-color: ${primary.main};
+        --secondary-main: ${secondary.main};
+        --secondary-light: ${secondary.light};
+        --secondary-dark: ${secondary.dark};
+        --secondary-contrastText: ${secondary.contrastText};
       }
-      .mui-btn.mui-btn--flat.mui-btn--primary { color: ${primary.main}; }
-
-      .mui-tabs__bar>li.mui--is-active { border-bottom-color: ${primary.main}; }
-      .mui-tabs__bar>li.mui--is-active>a { color: ${primary.main}; }
-
-      .mui-textfield > input:focus ~ label, .mui-textfield > textarea:focus ~ label { color: ${primary.main}; }
-      .mui-textfield > input:focus, .mui-textfield > textarea:focus { border-bottom-color: ${primary.main}; }
-
-      /* Accent */
-      .mui-btn--accent, .mui-btn--accent:active, .mui-btn--accent:hover, .mui-btn--accent:focus {
-        background-color: ${secondary.main};
-      }
-      .mui-btn.mui-btn--flat.mui-btn--accent { color: ${secondary.main}; }
     `;
 
-    document.getElementsByTagName('head')[0].appendChild(whiteLabelStyle);
+    document.getElementsByTagName('head')[0].appendChild(themeStyle);
   }
-}
+};
 
 (async () => {
   await app.initialize();
   const context = app.getContext();
-  generateWhitelabel(context?.app?.theme);
-})()
+  applyTheme(context?.app?.theme);
+})();
