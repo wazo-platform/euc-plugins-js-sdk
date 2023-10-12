0dc57bb882e63ce9d1304f86fe89b114377b1453
Emmanuel Quentin <manu.quentin@gmail.com>
2023-10-11 20:15:32 -0400

    Build app before unit tests in CI

--- test/browser/browser.spec.js
+++ test/browser/browser.spec.js
@@ -1,6 +1,15 @@
-import { test, expect } from '@playwright/test';
+import http from 'http';
+
+import { test } from '@playwright/test';
+import handler from 'serve-handler';
+
+const server = http.createServer((request, response) => {
+  return handler(request, response);
+});
 
 test('Importing library in browser does not throw any error', async ({ page }) => {
+  server.listen(3000);
+
   page.on('console', msg => {
     if (msg.type() === 'error') {
       throw new Error(msg.text());
@@ -10,4 +19,5 @@ test('Importing library in browser does not throw any error', async ({ page }) =
   await page.goto('http://localhost:3000/test/browser');
 
   await new Promise(resolve => setTimeout(resolve, 500));
+  server.close();
 });
--- test/src.spec.ts
+++ test/src.spec.ts
@@ -1,12 +1,13 @@
-// @TODO: Find a way to make this work with nodejs
 // import { app } from '../src';
-//
+
 // window.parent.postMessage = jest.fn();
-//
-// test('expected message returned from a Common JS import', () => {
-//   app._initializeCompleted = true;
-//   app.startCall({ targets: ['*10'], requestedModalities: ['video'] });
-//
-//   const message = { type: 'wazo/START_CALL', targets: ['*10'], requestedModalities:  ['video'], _pluginId: null, _entityId: null };
-//   expect(window.parent.postMessage).toHaveBeenCalledWith(message, '*');
-// })
+
+test('expected message returned from source', () => {
+  // @TODO: Find a way to make this work with nodejs
+
+  // app._initializeCompleted = true;
+  // app.startCall({ targets: ['*10'], requestedModalities: ['video'] });
+  //
+  // const message = { type: 'wazo/START_CALL', targets: ['*10'], requestedModalities:  ['video'], _pluginId: null, _entityId: null };
+  // expect(window.parent.postMessage).toHaveBeenCalledWith(message, '*');
+})
