3c870968c9acaa6d3f49a42acd25df1a89abb071
Francis Chartrand <chartrandf@users.noreply.github.com>
2024-09-16 09:07:58 -0400

    build: redirect to developers.wazo.io

--- examples/portal/pbx-user-form-tab/tab.js
+++
@@ -1,9 +0,0 @@
-import { App } from 'https://unpkg.com/@wazo/euc-plugins-sdk@latest/lib/esm/app.js';
-const app = new App();
-
-(async () => {
-  await app.initialize();
-  const context = app.getContext();
-
-  document.getElementById('name').innerHTML = context.app.extra.record.auth.username;
-})();
--- examples/wda/sidebar-color/tab.js
+++
@@ -1,9 +0,0 @@
-import { App } from 'https://unpkg.com/@wazo/euc-plugins-sdk@latest/lib/esm/app.js';
-
-const app = new App();
-
-(async () => {
-  await app.initialize();
-  app.closeLeftPanel();
-  app.changeNavBarColor('#8e6a3a');
-})();
